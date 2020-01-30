2daf8e276651072c0d7d2726f2b7625bf51927ae
Joe Shanahan <shans.joe@gmail.com>
2020-01-29 19:04:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
-var express = require('express');
-var app = express();
-var server = app.listen(process.env.PORT || 8080);
-function listen() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
-}
-app.use(express.static('public'));
-/*const http = require('http');
+const http = require('http');
 
-const hostname = '192.168.1.2';
+const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -20,4 +11,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});*/
+});
