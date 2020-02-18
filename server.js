788978ea61e3ba33cbf4983b02691cb8d8f6788d
Joe Shanahan <shans.joe@gmail.com>
2020-02-17 20:54:26 -0500

    fd

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const hostname = process.env.port;
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 server.listen(process.env.PORT);
